27a1bb4590e31132429963aa30732a52d38285d5
Tuan <tomasz.pilawa@gmail.com>
2022-02-16 15:55:31 +0100

    Create Table Stations Function

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,23 @@
 import sqlite3
 
+columns_stations = ['id', 'stationName', 'gegrLat', 'gegrLon', 'addressStreet', 'city_id', 'city_name', 'communeName',
+                    'districtName', 'provinceName']
 
-def create_new_table(table_name):
+
+def create_table_stations():
     conn = sqlite3.connect('air_quality.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE')
+    c.execute('''CREATE TABLE IF NOT EXISTS stations (
+    id INTEGER,
+    stationName TEXT,
+    gegrLat REAL,
+    gegrLon REAL,
+    addressStreet TEXT,
+    city_id INTEGER,
+    city_name TEXT,
+    communeName TEXT,
+    districtName TEXT,
+    provinceName TEXT,
+    ''')
+    conn.commit()
+    conn.close()
