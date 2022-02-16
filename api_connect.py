1a92448c9413f2bbc5747eef71b7e74577446255
Tuan <tomasz.pilawa@gmail.com>
2022-02-16 15:40:02 +0100

    Flatten Station and get nice Json List

--- api_connect.py
+++ api_connect.py
@@ -1,10 +1,24 @@
 import requests
+import pandas as pd
+import json
+
+pd.set_option('display.max_columns', None)
 
 all_stations = 'https://api.gios.gov.pl/pjp-api/rest/station/findAll'
 
 response = requests.get(all_stations)
 print(response.status_code)
 
-print(response.json())
+jos = response.json()
 
+df = pd.json_normalize(jos, sep='_')
+df = df.rename(columns={'city_commune_communeName': 'communeName', 'city_commune_districtName': 'districtName',
+                        'city_commune_provinceName': 'provinceName'})
 
+print(df)
+
+print(list(df.columns))
+
+json_list = json.loads(json.dumps(list(df.T.to_dict().values())))
+print(json_list)
+print(json_list[0])
